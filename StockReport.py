812f847fb33ddf1937e97f1a3af01e86eaa3bc68
ahmedimf1 <60337675+ahmedimf1@users.noreply.github.com>
2021-02-08 15:41:37 +0900

    Update StockReport.py

--- StockReport.py
+++ StockReport.py
@@ -1,10 +1,5 @@
-!pip install yfinance;
-!pip install datapane;
-import yfinance as yf
 import pandas as pd
-import plotly.graph_objects as go
 import numpy as np
-
 import plotly.figure_factory as ff
 import datapane as dp
 import plotly.offline as py
@@ -13,21 +8,20 @@ import plotly.express as px
 import pandas_datareader.data as web
 import datetime
 
+# py.init_notebook_mode(connected=True)
 
 import plotly.io as pio
 pio.templates.default = "plotly_dark"
-tickers = yf.Tickers('nflx msft AAPL goog GME')
+
+start = datetime.datetime(2019, 8, 1)
+end = datetime.datetime.now()
+
+zm = web.DataReader("ZM", 'yahoo', start, end).reset_index()
+nflx = web.DataReader("NFLX", 'yahoo', start, end).reset_index()
 
 
-nflx = tickers.tickers.NFLX.history(period="1y")
-aapl = tickers.tickers.AAPL.history(period="1y")
-gme = tickers.tickers.GME.history(period="1y")
 
-nflx ['10-day MA'] =  nflx.iloc[:,3].rolling(window=10).mean()
-nflx ['20-day MA'] =  nflx.iloc[:,3].rolling(window=20).mean()
-nflx ['50-day MA'] =  nflx.iloc[:,3].rolling(window=50).mean()
-
-trace0 = go.Scatter(x=nflx.index, y=nflx["Close"], name='nflx')
+trace0 = go.Scatter(x=nflx.Date, y=nflx.Close, name='nflx')
 fig0 = go.Figure([trace0])
 fig0.update_layout(
     title={
@@ -35,40 +29,49 @@ fig0.update_layout(
         'x':0.5,
         'xanchor': 'center'})
 
-trace0 = go.Scatter(x=nflx.index, y=nflx.Close, name='NFLX')
-trace1 = go.Scatter(x=nflx.index, y=nflx['10-day MA'], name='10-day MA')
-trace2 = go.Scatter(x=nflx.index, y=nflx['20-day MA'], name='20-day MA')
+
+
+nflx['10-day MA'] = nflx['Close'].rolling(window=10).mean()
+nflx['20-day MA'] = nflx['Close'].rolling(window=20).mean()
+nflx['50-day MA'] = nflx['Close'].rolling(window=50).mean()
+
+trace0 = go.Scatter(x=nflx.Date, y=nflx.Close, name='NFLX')
+trace1 = go.Scatter(x=nflx.Date, y=nflx['10-day MA'], name='10-day MA')
+trace2 = go.Scatter(x=nflx.Date, y=nflx['20-day MA'], name='20-day MA')
 fig1 = go.Figure([trace0, trace1, trace2])
 fig1.update_layout(
     title={
         'text': "Netflix Stock Price",
         'x':0.5,
         'xanchor': 'center'})
-        
-fig2 = go.Figure(go.Candlestick(x=nflx.index, open=nflx["Open"], high=nflx["High"], low=nflx["Low"], close=nflx["Close"]))
+
+
+fig2 = go.Figure(go.Candlestick(x=nflx.Date, open=nflx.Open, high=nflx.High, low=nflx.Low, close=nflx.Close))
 fig2.update_layout(
     title={
         'text': "Netflix Stock Price (Candle Stick)",
         'x':0.5,
         'xanchor': 'center'})
-fig2.show()
+
+
 
 nflx['Daily return (%)'] = round(nflx['Close'].pct_change()*100, 2)
 
-fig3 = px.bar(nflx, x=nflx.index, y="Daily return (%)")
+fig3 = px.bar(nflx, x="Date", y="Daily return (%)")
 fig3.update_layout(
     title={
         'text': "Netflix Stock Daily Return",
         'x':0.5,
         'xanchor': 'center'})
-fig3.show()
 
-trace1 = go.Scatter(x=aapl.index, y=aapl["Close"], name='AAPL', line=dict(color='red'))
+
+trace0 = go.Scatter(x=nflx.Date, y=nflx.Close, name='NFLX', line=dict(color='lime'))
+trace1 = go.Scatter(x=zm.Date, y=zm.Close, name='ZM', line=dict(color='red'))
 
 fig4 = go.Figure([trace0, trace1])
 fig4.update_layout(dict(
     title=dict({
-        'text': "Netflix vs Apple Stock Price",
+        'text': "Netflix vs Zoom Stock Price",
         'x':0.5,
         'xanchor': 'center'}),
     xaxis=dict(
@@ -96,7 +99,7 @@ fig4.update_layout(dict(
             visible = True
         ),
         type='date')))
-fig4.show()
+
 
 trace0 = go.Scatterpolar(
       r = [nflx['Close'].mean(),nflx['Open'].min(), nflx['Low'].min(),nflx['High'].max()],
@@ -105,9 +108,9 @@ trace0 = go.Scatterpolar(
       fill = 'toself')
 
 trace1 = go.Scatterpolar(
-      r = [aapl['Close'].mean(),aapl['Open'].min(), aapl['Low'].min(),aapl['High'].max()],
+      r = [zm['Close'].mean(),zm['Open'].min(), zm['Low'].min(),zm['High'].max()],
       theta = ['Close','Open','Low','High'], line =  dict(
-            color = 'red'), name='AAPL',
+            color = 'red'), name='ZM',
       fill = 'toself'
     )
 
@@ -117,19 +120,17 @@ fig5.update_layout(go.Layout(
         radialaxis = dict(
           visible = True)),
             title=dict({
-        'text': "Netflix vs Apple Stock Price (Radar Chart)",
+        'text': "Netflix vs Zoom Stock Price (Radar Chart)",
         'x':0.5,
         'xanchor': 'center'})))
 
 
-end = datetime.datetime.now()
-start = datetime.datetime(end.year-1,end.month,end.day)
-df = web.DataReader(['NFLX', 'FB', 'AAPL', 'GME'], 'yahoo', start, end)['Close']
+df = web.DataReader(['BABA', 'FB', 'AAPL', 'SPOT'], 'yahoo', start, end)['Close']
 
-trace0 = go.Box(y=df.FB, name='FB', line=dict(color='blue'))
-trace1 = go.Box(y=df.NFLX, name='NFLX', line=dict(color='red'))
-trace2 = go.Box(y=df.AAPL, name='AAPL', line=dict(color='silver'))
-trace3 = go.Box(y=df.GME, name='GME', line=dict(color='purple'))
+trace0 = go.Box(y=df.BABA, name='BABA', line=dict(color='blue'))
+trace1 = go.Box(y=df.FB, name='FB', line=dict(color='red'))
+trace2 = go.Box(y=df.AAPL, name='AAPL', line=dict(color='green'))
+trace3 = go.Box(y=df.SPOT, name='SPOT', line=dict(color='purple'))
 
 fig6 = go.Figure([trace0, trace1, trace2, trace3])
 fig6.update_layout(
@@ -147,6 +148,7 @@ fig7.update_layout(
         'xanchor': 'center'})
 
 
+
 nflx = nflx[['Open','Close','Volume']]
 nflx["index"] = np.arange(len(nflx))
 
@@ -156,9 +158,6 @@ fig8 = go.Figure(ff.create_scatterplotmatrix(nflx, diag='box', index='index',siz
                                     'text': "Netflix Stock Price (Scatterplot Matrix)",
                                     'x':0.5,
                                     'xanchor': 'center'}))
-
-
-
 
 dp.Report(
     dp.Group(
@@ -174,4 +173,3 @@ dp.Report(
         rows=4
     ), dp.Plot(fig8)
 ).publish(name='stock_report', open=True)
-
